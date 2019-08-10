be2b71a53b80e5daf32b8da43fc0b14368c43fc9
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-08-11 00:14:05 +0200

    #1 Test the detection of all the close button ok

--- SAIEyes.py
+++ SAIEyes.py
@@ -4,3 +4,9 @@ import pyautogui
 class SAIEyes:
     def get_current_screen(self):
         return pyautogui.screenshot('Eyes\current_vision.png')
+
+    def get_pos_all_close_button(self):
+        return pyautogui.locateAllOnScreen('Memory\close_button_window.png')
+
+    def move_to(self, x, y, duration=5):
+        return pyautogui.moveTo(x, y, duration)
--- test_core_functions.py
+++ test_core_functions.py
@@ -1,5 +1,7 @@
 import unittest
 import SAIEyes
+import docker
+
 
 class TestCoreFunctions(unittest.TestCase):
     def test_0(self):
@@ -7,9 +9,25 @@ class TestCoreFunctions(unittest.TestCase):
         pred = 0
         assert(pred == res)
 
-    def test_SIAEyes_get_screen(self):
+    def test_SAIEyes_get_screen(self):
         saie = SAIEyes.SAIEyes()
         print(saie.get_current_screen())
+
+    def test_SAIEyes_locate_screen(self):
+        saie = SAIEyes.SAIEyes()
+        all_close_button = list(saie.get_pos_all_close_button())
+        for close_button in all_close_button:
+            x, y, width, height = close_button
+            print(all_close_button[0])
+            saie.move_to(x + width/2, y + height/2)
+
+    def test_docker_ubuntu(self):
+        client = docker.from_env()
+        print(client)
+        #client.images.pull('ubuntu')
+        #for image in client.images.list():
+        #    print(image.id)
+        #print(client.containers.run("ubuntu", "echo hello world"))
 
 
 if __name__ == '__main__':
