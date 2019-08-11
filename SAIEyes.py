5d56ceea97c5dd197413ccb3c7907a8856961470
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-08-11 07:28:11 +0200

    #1 Test the detection of all the close button with the court term memory ok

--- SAIEyes.py
+++ SAIEyes.py
@@ -1,12 +1,59 @@
 import pyautogui
+import os
+from PIL import Image, ImageDraw
+import glob
+
+import logging
 
 
 class SAIEyes:
-    def get_current_screen(self):
-        return pyautogui.screenshot('Eyes\current_vision.png')
+    def __init__(self, eyes_dir="Eyes", ltm_dir="LongTermMemory", ctm_dir="CourtTermMemory", level=logging.INFO):
+        self.eyes_dir = eyes_dir
+        self.ltm_dir = ltm_dir
+        self.ctm_dir = ctm_dir
+        logging.basicConfig(level=level)
 
-    def get_pos_all_close_button(self):
-        return pyautogui.locateAllOnScreen('Memory\close_button_window.png')
+    def get_current_screen(self, cur_screen="current_vision.png"):
+        logging.info(os.path.join(self.eyes_dir, cur_screen))
+        return pyautogui.screenshot(os.path.join(self.eyes_dir, cur_screen))
+
+    def get_pos_all_close_button(self, cur_button="close_button_window.png"):
+        logging.info(os.path.join(self.ltm_dir, cur_button))
+        return pyautogui.locateAllOnScreen(os.path.join(self.ltm_dir, cur_button))
+
+    def save_image_court_term_memory(self, img, title="test.png"):
+        logging.info(os.path.join(self.ctm_dir, title))
+        img.save(os.path.join(self.ctm_dir, title))
+
+    def add_rectangle_on_image(self, img, _box):
+        new_img = ImageDraw.Draw(img)
+        x0, y0,x1, y1=_box
+        new_img.rectangle([(x0, y0), (x1, y1)], outline="red")
+
+    def save_image_with_rectangle_court_term_memory(self, _box, cur_screen="current_vision.png", title="test_rectangle.png"):
+        logging.info(os.path.join(self.eyes_dir, cur_screen))
+        img = Image.open(os.path.join(self.eyes_dir, cur_screen)).convert("RGBA")
+        self.add_rectangle_on_image(img, _box)
+        logging.info("New img to save")
+        img.save(os.path.join(self.ctm_dir, title))
+
+    def save_image_with_rectangles_court_term_memory(self, _boxes, cur_screen="current_vision.png", title="test_rectangles.png"):
+        logging.info(os.path.join(self.eyes_dir, cur_screen))
+        img = Image.open(os.path.join(self.eyes_dir, cur_screen)).convert("RGBA")
+        for _box in _boxes:
+            x, y, w, h = _box
+            self.add_rectangle_on_image(img, (x, y, x+w, y+h))
+        logging.info("New img to save")
+        img.save(os.path.join(self.ctm_dir, title))
 
     def move_to(self, x, y, duration=5):
         return pyautogui.moveTo(x, y, duration)
+
+    def clean_ctm(self):
+        files = glob.glob(os.path.join(self.ctm_dir, '*'))
+        for f in files:
+            os.remove(f)
+
+    def count_ctm_files(self):
+        files = glob.glob(os.path.join(self.ctm_dir, '*'))
+        return len(files)
--- test_core_functions.py
+++ test_core_functions.py
@@ -7,7 +7,7 @@ class TestCoreFunctions(unittest.TestCase):
     def test_0(self):
         res = 0
         pred = 0
-        assert(pred == res)
+        assert (pred == res)
 
     def test_SAIEyes_get_screen(self):
         saie = SAIEyes.SAIEyes()
@@ -18,16 +18,53 @@ class TestCoreFunctions(unittest.TestCase):
         all_close_button = list(saie.get_pos_all_close_button())
         for close_button in all_close_button:
             x, y, width, height = close_button
-            print(all_close_button[0])
+            print(close_button)
             saie.move_to(x + width/2, y + height/2)
+
+    def test_SAIEyes_crop_locate_screen(self):
+        saie = SAIEyes.SAIEyes()
+        all_close_button_generator = saie.get_pos_all_close_button()  # 0.052 s
+        screen = saie.get_current_screen()  # 0.198 s
+        try:
+            x, y, w, h = next(all_close_button_generator)
+            cropped_example = screen.crop((x, y, x+w, y+h))
+            saie.save_image_court_term_memory(cropped_example)
+        except Exception as e:
+            print("No button found", e)
+
+    def test_SAIEyes_rectangle_locate_screen(self):
+        saie = SAIEyes.SAIEyes()
+        all_close_button_generator = saie.get_pos_all_close_button()  # 0.052 s
+        screen = saie.get_current_screen()  # 0.198 s
+        try:
+            saie.clean_ctm()
+            x, y, w, h = next(all_close_button_generator)
+            saie.save_image_with_rectangle_court_term_memory((x, y, x+w, y+h))
+        except Exception as e:
+            print("No button found", e)
+
+    def test_SAIEyes_rectangles_locate_screen(self):
+        saie = SAIEyes.SAIEyes()
+        all_close_button_generator = saie.get_pos_all_close_button()  # 0.052 s
+        screen = saie.get_current_screen()  # 0.198 s
+        try:
+            saie.clean_ctm()
+            saie.save_image_with_rectangles_court_term_memory(all_close_button_generator)
+        except Exception as e:
+            print("No button found", e)
+
+    def test_SAIEyes_clean_ctm(self):
+        saie = SAIEyes.SAIEyes()
+        saie.clean_ctm()
+        assert saie.count_ctm_files() == 0
 
     def test_docker_ubuntu(self):
         client = docker.from_env()
         print(client)
-        #client.images.pull('ubuntu')
-        #for image in client.images.list():
+        # client.images.pull('ubuntu')
+        # for image in client.images.list():
         #    print(image.id)
-        #print(client.containers.run("ubuntu", "echo hello world"))
+        # print(client.containers.run("ubuntu", "echo hello world"))
 
 
 if __name__ == '__main__':
