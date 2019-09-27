e0f3ecd04a7a8c4e085e6ac5a8bc882cefbe437d
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-09-27 13:12:14 +0200

    #10 SAICommand constructor must be implement

---
+++ SAICommand.py
@@ -0,0 +1,12 @@
+class SAICommand:
+    """
+    This class allow the SAI Brain to launch a command
+    """
+    def __init__(self, ):
+        """
+        This parameters are used to create a command:
+            - it can be a table with a serie of move, click and time between each actions
+        """
+        # TODO implement
+        pass
+
