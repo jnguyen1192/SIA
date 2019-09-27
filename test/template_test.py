1bdb5693ddc204dfb78288e6c08b36e4d2826c36
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-09-27 13:23:50 +0200

    #11 Template test file added

---
+++ test/template_test.py
@@ -0,0 +1,15 @@
+import unittest
+import SAIX
+
+
+class TestSAIX(unittest.TestCase):
+
+    def setUp(self):
+        self.said = SAIX.SAIX()
+
+    def test_X(self):
+        assert (True==True)
+
+
+if __name__ == '__main__':
+    unittest.main()
