cec5dd9eb7637baade151e6c50a5ce36326c1cfd
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-09-27 13:03:10 +0200

    #10 Add main methods on SAI Brain

---
+++ SAIBrain.py
@@ -0,0 +1,26 @@
+class SAIBrain:
+    """
+    This class will manipulate data to learn things or manage memory
+    """
+    def __init__(self, nb_best_command=5, nb_time_rest_second=3600):
+        """
+        It will set the core of the SIA
+        """
+        self.nb_best_command = nb_best_command
+        self.nb_time_rest_second = nb_time_rest_second
+
+    def rest(self):
+        """
+        It will rest and manage the memory
+        :return: 0 if it works else -1
+        """
+        # TODO implement
+        pass
+
+    def find_new_command(self):
+        """
+        It will search a new command
+        :return: 0 if it works else -1
+        """
+        # TODO implement
+        pass
