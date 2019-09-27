b80fcaaa32495379acb189e2c6dce22d5e300977
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-09-27 13:26:56 +0200

    #10 Setup added to SAIBrain test

---
+++ test/test_sai_brain.py
@@ -0,0 +1,12 @@
+import unittest
+import SAIBrain
+
+
+class TestSAIBrain(unittest.TestCase):
+
+    def setUp(self):
+        self.said = SAIBrain.SAIBrain()
+
+
+if __name__ == '__main__':
+    unittest.main()
