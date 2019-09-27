49714566c09f6018efe51c2fbc72f4059a238e79
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-09-27 13:30:55 +0200

    #10 Header of optional methods test functions added

--- test/test_sai_brain.py
+++ test/test_sai_brain.py
@@ -23,6 +23,23 @@ class TestSAIBrain(unittest.TestCase):
         # TODO implement
         assert (False==False)
 
+    """ Optionnal methods"""
+    def test_SAIBrain_is_command_exist_case_true(self):
+        # TODO implement
+        assert (True==True)
+
+    def test_SAIBrain_is_command_exist_case_false(self):
+        # TODO implement
+        assert (False==False)
+
+    def test_SAIBrain_manage_memory_case_ok(self):
+        # TODO implement
+        assert (True==True)
+
+    def test_SAIBrain_manage_memory_case_nok(self):
+        # TODO implement
+        assert (False==False)
+
 
 if __name__ == '__main__':
     unittest.main()
