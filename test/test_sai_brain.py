53da57fddd73ac8187840589371112fdb1daac13
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-09-27 13:27:59 +0200

    #10 Header of rest test function added

--- test/test_sai_brain.py
+++ test/test_sai_brain.py
@@ -7,6 +7,14 @@ class TestSAIBrain(unittest.TestCase):
     def setUp(self):
         self.said = SAIBrain.SAIBrain()
 
+    def test_SAIBrain_rest_case_ok(self):
+        # TODO implement
+        assert (True==True)
+
+    def test_SAIBrain_rest_case_nok(self):
+        # TODO implement
+        assert (False==False)
+
 
 if __name__ == '__main__':
     unittest.main()
