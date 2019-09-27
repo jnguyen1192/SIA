027fa5716b59318dd796dcbd902cd7e0671e195f
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-09-27 13:28:52 +0200

    #10 Header of find_new_command test function added

--- test/test_sai_brain.py
+++ test/test_sai_brain.py
@@ -15,6 +15,14 @@ class TestSAIBrain(unittest.TestCase):
         # TODO implement
         assert (False==False)
 
+    def test_SAIBrain_find_new_command_case_ok(self):
+        # TODO implement
+        assert (True==True)
+
+    def test_SAIBrain_find_new_command_case_nok(self):
+        # TODO implement
+        assert (False==False)
+
 
 if __name__ == '__main__':
     unittest.main()
