13d59b141ac79aca0108b19bacd39c98caf16943
Nguyen <johdu13@hotmail.fr>
2019-09-30 12:34:27 +0200

    #10 Function query with parameters

--- tools/db_tools.py
+++ tools/db_tools.py
@@ -1,5 +1,4 @@
 import psycopg2
-from psycopg2 import Error
 
 
 # TODO create function to use a query without parameters
@@ -21,7 +20,7 @@ def query_without_parameters(query_without_parameters):
 
         cursor.execute(query_without_parameters)
         connection.commit()
-        print("Query executed successfully in PostgreSQL ")
+        print("Query without parameters executed successfully in PostgreSQL ")
         return 0
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error while executing query in PostgreSQL", error)
@@ -33,4 +32,34 @@ def query_without_parameters(query_without_parameters):
             connection.close()
             print("PostgreSQL connection is closed")
 
+
 # TODO create function to use a query with parameters
+def query_wit_parameters(query, parameters):
+    """
+    Create a query on the database without parameters
+    :return: 0 if it works else -1
+    """
+    connection = ""
+    cursor = ""
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="pass@#29",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres_db")
+        cursor = connection.cursor()
+
+
+        cursor.execute(query, parameters)
+        connection.commit()
+        print("Query with parameters executed successfully in PostgreSQL ")
+        return 0
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error while executing query with parameters in PostgreSQL", error)
+        return -1
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
