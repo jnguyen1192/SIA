35057cc9c792c0ec9b38964af2d19facb715e97e
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-01 15:14:03 +0200

    #10 Test function run_db on db_tools

--- test/test_db_tools.py
+++ test/test_db_tools.py
@@ -4,6 +4,14 @@ import tools.db_tools as dbt
 
 class Testdb_tools(unittest.TestCase):
 
+    def test_db_tools_run_db_case_ok(self):
+        # TODO implement
+        assert (True == True)
+
+    def test_db_tools_run_db_case_nok(self):
+        # TODO implement
+        assert (False == False)
+
     def test_db_tools_query_without_parameters_case_ok(self):
         # TODO implement
         assert (True == True)
--- tools/db_tools.py
+++ tools/db_tools.py
@@ -1,6 +1,26 @@
 import psycopg2
+import docker
+
 
 # TODO create the container with its credentials
+def run_db():
+    """
+    Create the postgres container and run it
+    :return: 0 if it works else -1
+    """
+    try:
+        client = docker.from_env()
+        volumes = {"/c/Users/johdu/PycharmProjects/SAI/data_postgres":
+                       {'bind': '/var/lib/postgresql/data/', 'mode': 'rw'}
+                   }
+
+        print(client.containers.run(image="postgres",
+                                    name="c_sai_postgres",
+                                    volumes=volumes).decode('utf8'))
+        return 0
+    except Exception as e:
+        print(e)
+        return -1
 
 # TODO connect to the database with the correct credentials and refactor
 
