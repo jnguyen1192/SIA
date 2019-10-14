f4d924a40b054b629887c44f599d7327d3707df2
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-14 17:02:28 +0200

    #13 Init test for sai_hands class

---
+++ test/test_sai_hands.py
@@ -0,0 +1,13 @@
+import unittest
+
+import SAIHands
+
+
+class TestSAIHands(unittest.TestCase):
+
+    def setUp(self):
+        self.saih = SAIHands.SAIHands()
+
+
+if __name__ == '__main__':
+    unittest.main()
