eff96d497ad4a63d8ac21ef7e37c8b4cc827eaf3
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-14 17:04:13 +0200

    #13 Header of the first tests of sai_hands class

--- test/test_sai_hands.py
+++ test/test_sai_hands.py
@@ -8,6 +8,14 @@ class TestSAIHands(unittest.TestCase):
     def setUp(self):
         self.saih = SAIHands.SAIHands()
 
+    def test_SAIHands_move_mouse_to_OK(self):
+        # TODO implement
+        pass
+
+    def test_SAIHands_move_mouse_to_NOK(self):
+        # TODO implement
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
