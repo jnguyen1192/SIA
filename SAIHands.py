a50b9c6a6b9cd0cb97cda2c1c30e90b1c4f1ab24
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-14 16:59:52 +0200

    #13 Refactor from SAIEyes to SAIHands using move from mouse

--- SAIHands.py
+++ SAIHands.py
@@ -4,4 +4,21 @@ import pyautogui
 class SAIHands:
     """
     This class is used to use mouse, keyboard or another type of control
+    First we will only use mouse and keyboard
     """
+    def move_mouse_to(self, x, y, duration=5):
+        """
+        The mouse will move in the point we choose
+        :param x: x axis
+        :param y: y axis
+        :param duration: time to move to the point
+        :return: 0 if it works else -1
+        """
+        try:
+            pyautogui.moveTo(x, y, duration)
+        except Exception as e:
+            print("move_mouse_to :", e)
+            return -1
+        return 0
+
+
