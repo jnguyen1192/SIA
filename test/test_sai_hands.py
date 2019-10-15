817b8947c4746ea827c5683dd48f8e0735f79b31
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-15 09:29:26 +0200

    #13 Last update first test of sai_hands class

--- test/test_sai_hands.py
+++ test/test_sai_hands.py
@@ -9,13 +9,11 @@ class TestSAIHands(unittest.TestCase):
         self.saih = SAIHands.SAIHands()
 
     def test_SAIHands_move_mouse_to_OK(self):
-        x_old = self.saih.mouse_position().x
-        y_old = self.saih.mouse_position().y
+        p_old = self.saih.mouse_position()
         self.saih.move_mouse_to(10, 10, 0)
-        x = self.saih.mouse_position().x
-        y = self.saih.mouse_position().y
-        self.saih.move_mouse_to(x_old, y_old, 0)
-        assert((10, 10) == (x, y))
+        p = self.saih.mouse_position()
+        self.saih.move_mouse_to(p_old.x, p_old.y, 0)
+        assert((10, 10) == (p.x, p.y))
 
     def test_SAIHands_move_mouse_to_NOK(self):
         # TODO implement
