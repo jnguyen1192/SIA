43204dadc4cbce845b23f7c45f29ecefaa9ff31b
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-15 09:31:10 +0200

    #13 Second test of sai_hands class implemented

--- test/test_sai_hands.py
+++ test/test_sai_hands.py
@@ -16,8 +16,11 @@ class TestSAIHands(unittest.TestCase):
         assert((10, 10) == (p.x, p.y))
 
     def test_SAIHands_move_mouse_to_NOK(self):
-        # TODO implement
-        pass
+        p_old = self.saih.mouse_position()
+        self.saih.move_mouse_to(p_old.x, p_old.y, 0)
+        p = self.saih.mouse_position()
+        self.saih.move_mouse_to(p_old.x, p_old.y, 0)
+        assert((10, 10) != (p.x, p.y))
 
 
 if __name__ == '__main__':
