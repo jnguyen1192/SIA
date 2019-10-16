d3a537814d6bd2bfa42fffd090aa1f02e69c6703
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-16 05:24:24 +0200

    #13 Test key press functions header added

--- test/test_sai_hands.py
+++ test/test_sai_hands.py
@@ -9,6 +9,7 @@ class TestSAIHands(unittest.TestCase):
         self.saih = SAIHands.SAIHands()
 
     def test_SAIHands_move_mouse_to_OK(self):
+
         p_old = self.saih.mouse_position()
         self.saih.move_mouse_to(10, 10, 0)
         p = self.saih.mouse_position()
@@ -21,6 +22,20 @@ class TestSAIHands(unittest.TestCase):
         p = self.saih.mouse_position()
         self.saih.move_mouse_to(p_old.x, p_old.y, 0)
         assert((10, 10) != (p.x, p.y))
+
+    def test_SAIHands_press_keyboard_to_OK(self):
+        """
+        Test if the keyboard works with every keys
+        """
+        # TODO immplement
+        pass
+
+    def test_SAIHands_press_keyboard_to_NOK(self):
+        """
+        Test if the keyboard not works with a wrong key
+        """
+        # TODO immplement
+        pass
 
 
 if __name__ == '__main__':
