2fa5a7a4834fc92547d44ed0951cd5ca3f21047e
jnguyen1192 <jnguyen1192@users.noreply.github.com>
2019-10-16 05:26:22 +0200

    #13 Comment added on mouse move functions

--- test/test_sai_hands.py
+++ test/test_sai_hands.py
@@ -8,15 +8,20 @@ class TestSAIHands(unittest.TestCase):
     def setUp(self):
         self.saih = SAIHands.SAIHands()
 
-    def test_SAIHands_move_mouse_to_OK(self):
-
+    def test_SAIHands_move_mouse_to_OK_1(self):
+        """
+        Test if the mouse moves correctly first way
+        """
         p_old = self.saih.mouse_position()
         self.saih.move_mouse_to(10, 10, 0)
         p = self.saih.mouse_position()
         self.saih.move_mouse_to(p_old.x, p_old.y, 0)
         assert((10, 10) == (p.x, p.y))
 
-    def test_SAIHands_move_mouse_to_NOK(self):
+    def test_SAIHands_move_mouse_to_OK_2(self):
+        """
+        Test if the mouse moves correctly second way
+        """
         p_old = self.saih.mouse_position()
         self.saih.move_mouse_to(p_old.x, p_old.y, 0)
         p = self.saih.mouse_position()
