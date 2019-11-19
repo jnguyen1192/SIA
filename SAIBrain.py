46310a4a9dd1677a9849f2e482dc438f8e6e85ae
jnguyen1192 <johdu13@hotmail.fr>
2019-11-19 10:47:47 +0100

    #11 Last instruction to function create_new_shape added

--- SAIBrain.py
+++ SAIBrain.py
@@ -136,6 +136,10 @@ class SAIBrain:
         # Use the class shape to create a new shape
         # This shape will be on a new image in the directory almost long term memory
         # This image will be compose of the shape as pixel (255, 255, 255, 255) and (0, 0, 0, 0)
+        # This image will have a special name using height_width_number:
+        #   - height : the height of the image
+        #   - width : the width of the image
+        #   - number : the number of the image in the corresponding cluster (height, width)
         return 0
 
     def is_in(self, point, all_polygon):
