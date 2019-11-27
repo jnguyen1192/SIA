1fb5db2d960970919231884068274ebc45107f80
jnguyen1192 <johdu13@hotmail.fr>
2019-11-27 10:14:58 +0100

    #4 9.242 seconds to take 26 screenshots, it may be less than one second

--- SpeedTest.py
+++ SpeedTest.py
@@ -1,15 +1,43 @@
 import time
+import unittest
 
 
-class SpeedTest:
+class SpeedTest(unittest.TestCase):
+
     def test_speed_once(self, func, *args):
         beg = time.time()
         func(*args)
         end = time.time()
         return end - beg
 
+    def test_speed_26_screenshot_in_one_second(self):
+        """
+        The goal is to mesurate how maximum can we take screen in less than a second
+        """
+        import threading
+        import time
+
+        import SAIEyes
+
+        saie = SAIEyes.SAIEyes(eyes_dir="Speedtest", ctm_dir="Speedtest")
+
+        def loop26():
+            for i in range(26):
+                name = "gross_"+str(i)
+                cs = saie.get_current_screen(name+".png")
+                saie.save_image_court_term_memory(cs, name + ".png")
+
+        my_thread = threading.Thread(target=loop26)
+        my_thread.start()
+
+        my_thread.join()
+
     def test_speed(self, func, *args, nb_test=10):
         sum = 0
         for i in range(nb_test):
             sum += self.test_speed_once(func, *args)
         return sum/nb_test
+
+
+if __name__ == '__main__':
+    unittest.main()
