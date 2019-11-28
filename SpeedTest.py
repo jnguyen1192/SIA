a6a3c6cb83c6b4dc3b469e5b00bedad27136e2a4
jnguyen1192 <johdu13@hotmail.fr>
2019-11-28 08:12:05 +0100

    #4 d3dshot library choose to take jpg screenshot (15 img/s)

--- SpeedTest.py
+++ SpeedTest.py
@@ -16,16 +16,35 @@ class SpeedTest(unittest.TestCase):
         """
         import threading
         import time
+        from mss import mss
+        import os
 
         import SAIEyes
 
         saie = SAIEyes.SAIEyes(eyes_dir="Speedtest", ctm_dir="Speedtest")
 
         def loop26():
+            import d3dshot
+            import os
+            d = d3dshot.create()
             for i in range(26):
                 name = "gross_"+str(i)
-                cs = saie.get_current_screen(name+".png")
-                saie.save_image_court_term_memory(cs, name + ".png")
+                print(name)
+                # TODO find the best way
+                # first way 9.029s => 2 img/s
+                #cs = saie.get_current_screen(name+".png")
+                #saie.save_image_court_term_memory(cs, name + ".png")
+                # second way 5.343s => 4 img/s as a jpg 2.68s => 10 img/s
+                #import pyscreeze
+                #pyscreeze._screenshot_win32(name+".png")
+                # third way 4.658s => 5 img/s
+                #with mss() as sct:
+                #    print(sct.shot())
+                    # rename monitor-1.png to name+".png"import os
+                #    os.replace('monitor-1.png', name+".png")
+                    #sct.save()
+                # fourth way 4.909 => 5 img/s as a jpg 15 img/s
+                d.screenshot_to_disk(directory=os.path.join("Speedtest", "CourtTermMemory"), file_name=name + ".png")
 
         my_thread = threading.Thread(target=loop26)
         my_thread.start()
