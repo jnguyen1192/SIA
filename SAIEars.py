2631b758ba1bd7d801a8d613a82c3e476d1ff795
jnguyen1192 <johdu13@hotmail.fr>
2020-01-02 16:34:28 +0100

    #16 Header of function get_command added

--- SAIEars.py
+++ SAIEars.py
@@ -7,4 +7,9 @@ class SAIEars:
     """
     # TODO use test_speech_recognition.py
 
-
+    def get_command(self):
+        """
+        Get the microphone input
+        :return: the input as a text
+        """
+        # TODO implement
