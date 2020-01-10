e420a42f4d248fb6d55ad4999008839b933acdef
jnguyen1192 <johdu13@hotmail.fr>
2020-01-10 09:19:36 +0100

    #16 Instructions to use microphone functions

--- test/test_sai_ears.py
+++ test/test_sai_ears.py
@@ -306,6 +306,38 @@ class TestSAIEars(unittest.TestCase):
         ear.close()
         print("DONE")
 
+    def test_SAIEars_analyse_microphone_6(self):
+        # TODO
+        #   Use those data to only record when there was a sound of a voice using dynamic deep reinforcement learning
+        import pyaudio
+        import numpy
+
+        RATE = 16000
+        RECORD_SECONDS = 2.5
+        CHUNKSIZE = 1024
+
+        # initialize portaudio
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNKSIZE)
+
+        frames = []  # A python-list of chunks(numpy.ndarray)
+        for _ in range(0, int(RATE / CHUNKSIZE * RECORD_SECONDS)):
+            data = stream.read(CHUNKSIZE)
+            frames.append(numpy.fromstring(data, dtype=numpy.int16))
+
+        # Convert the list of numpy-arrays into a 1D array (column-wise)
+        numpydata = numpy.hstack(frames)
+
+        # close stream
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        from pprint import pprint
+        pprint(numpydata)
+        import scipy.io.wavfile as wav
+        wav.write('out.wav', RATE, numpydata)
+        pprint(wav.read('out.wav'))
+        pprint(wav.read('out.wav')[1].shape)
 
     def test_create_actions_xml(self):
         """
